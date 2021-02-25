5767d5da86ef5c3932456496a434183a2d9b991a
Sena <NseCnaT@users.noreply.github.com>
2021-02-26 02:25:41 +0900

    カスタムセルの作成

--- Swift6-Kcommunity/MessageCell.swift
+++ Swift6-Kcommunity/MessageCell.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        rightImageView.layer.cornerRadius = 25.0
+        leftImageView.layer.cornerRadius = 25.0
+        backView.layer.cornerRadius = 10.0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
