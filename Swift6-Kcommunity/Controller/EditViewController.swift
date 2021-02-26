1b496a4539d4c7ef90af98dcd7dd8a419a44d792
Sena <NseCnaT@users.noreply.github.com>
2021-02-26 10:22:56 +0900

    commit

--- Swift6-Kcommunity/Controller/EditViewController.swift
+++ Swift6-Kcommunity/Controller/EditViewController.swift
@@ -7,15 +7,89 @@
 
 import UIKit
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var eventTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var detailTextField: UITextView!
+    
+    var sendDBModel = SendDBModel()
+    var urlString = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        eventTextField.delegate = self
+        dateTextField.delegate = self
+        detailTextField.delegate = self
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        eventTextField.resignFirstResponder()
+        dateTextField.resignFirstResponder()
+        detailTextField.resignFirstResponder()
     }
     
-
+    @IBAction func send(_ sender: Any) {
+        if eventTextField.text?.isEmpty != true && dateTextField.text?.isEmpty != true &&
+            detailTextField.text.isEmpty != true, let image = imageView.image{
+            
+        }
+    }
+    @IBAction func openCamera(_ sender: Any) {
+        showAlert()
+    }
+    func doCamera() {
+        let sourceType:UIImagePickerController.SourceType = .camera
+        //カメラ利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.allowsEditing = true
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+        }
+    }
+    func doAlbum() {
+        let sourceType:UIImagePickerController.SourceType = .photoLibrary
+        //アルバム利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.allowsEditing = true
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if info[.originalImage] as? UIImage != nil {
+            let selectedImage = info[.originalImage] as! UIImage
+            imageView.image = selectedImage
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    //アラート
+    func showAlert() {
+        let alertController = UIAlertController(title: "選択", message: "どちらを使用しますか?", preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "カメラ", style: .default) { (alert) in
+            self.doCamera()
+        }
+        let action2 = UIAlertAction(title: "アルバム", style: .default) { (alert) in
+            self.doAlbum()
+        }
+        let action3 = UIAlertAction(title: "キャンセル", style: .cancel)
+        
+        alertController.addAction(action1)
+        alertController.addAction(action2)
+        alertController.addAction(action3)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
